0b7793656bc9c23aefb7c663ee0a6041dab9193e
User of DK12 <DK12@it.isuct.ru>
2018-10-16 13:12:33 +0400

    lab2

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
-namespace CourseApp
+namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Formula(double x)
         {
-            Console.WriteLine("jurawlew mihail aleksandrovich");
-            Console.ReadLine();        }
+            double y = (Math.log(Math.Abs(Math.Pow(1.1, 2) - Math.Pow(x, 2))) / Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(2.0, 2)), 0.2);          
+            Console.WriteLine($"При начальном и конечном х = {x} функция y = {Math.Round(y, 4)}");
+        }
+        static void Main()
+        {
+            Console.WriteLine("1 вариант");
+            for (double x = 0.08; x <= 1.08; x = x + 0.2)
+            {
+                Formula(x);
+            }
+            Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
     }
 }
