f065965304dab71cf8a129d22e1c944b425f1a49
User of DK12 <DK12@it.isuct.ru>
2018-11-02 15:00:05 +0400

    test

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
-
-        static void Main()
+        public static double Formula(double x)
+        {
+            double a = 2.0;
+            double b = 1.1;
+            double y = Math.Log(Math.Abs(Math.Pow(b, a) - Math.Pow(x, 2))) / Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(a, 2)), 0.2);
+            return y;
+        }
+        public static void Main()
         {
             double[] z = new double[5] { 0.1, 0.3, 0.4, 0.45, 0.65 };
             Console.WriteLine("Задание А");
@@ -22,13 +28,7 @@ namespace ConsoleApp1
                 Console.WriteLine(Formula(z[i]));
             }
 
-            double Formula(double x)
-            {
-                double a = 2.0;
-                double b = 1.1;
-                double y = Math.Log(Math.Abs(Math.Pow(b, a) - Math.Pow(x, 2))) / Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(a, 2)), 0.2);
-                return y;
-            }
+          
             Console.WriteLine();
             Console.ReadKey();
         }
