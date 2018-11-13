6dbd175c14e1f9b440ab4b708819a4c14dfc4625
User of DK12 <DK12@it.isuct.ru>
2018-11-13 13:11:00 +0400

    test+

---
+++ CourseApp.Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+using System;
+using Xunit; 
+using ConsoleApp1;
+
+
+namespace CourseApp.Tests
+{
+    public class UnitTest1
+    {
+        double a = 2.0;
+        double b = 1.1;
+        double x1 = 0.08;
+        double x2 = 1.08;
+        double dx = 0.2;
+        double[] z = new double[5] { 0.1, 0.3, 0.4, 0.45, 0.65 };
+        [Fact]
+        public void Test1()
+        {
+            double y = Program.Formula(0.1, 2.0, 1.1);
+            Assert.Equal(0.138, y, 3);
+        }
+        [Fact]
+        public void Test2()
+        {
+            double y = Program.Formula(0, 2.0, 1.1);
+            Assert.Equal(0.144, y, 3);
+        }
+        [Fact]
+        public void Test3()
+        {
+            double y = Program.Formula(0.1, 0, 1.1);
+            Assert.Equal(-0.025, y, 3);
+        }
+        [Fact]
+        public void Test4()
+        {
+            double y = Program.Formula(0.1, 2.0, 0);
+            Assert.Equal(-3.492, y, 3);
+        }
+
+        [Fact]
+        public void test5()
+        {
+            double[] mas = new double[5] { 0.138, 0.086, 0.037, 0.006, -0.185 };
+            for (int i = 0; i < mas.Length; i++)
+            {
+                var y = Program.Formula(z[i],2.0, 1.1);
+                Assert.Equal(mas[i], y, 3);
+            }
+        }
+        [Fact]
+        public void test6()
+        {
+            int i = 0;
+            double[] mas = new double[6] { 0.140, 0.094, -0.016, -0.226, -0.657, -2.544 };
+            for (double j = x1; j <= x2; j += dx)
+            {
+                var y = Program.Formula(j, 2.0, 1.1);
+                Assert.Equal(mas[i], y, 3);
+                i++;
+            }
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -3,20 +3,23 @@ namespace ConsoleApp1
 {
     public class Program
     {
-        public static double Formula(double x)
+        public static double Formula(double x, double a, double b)
         {
-            double a = 2.0;
-            double b = 1.1;
+
             double y = Math.Log(Math.Abs(Math.Pow(b, a) - Math.Pow(x, 2))) / Math.Pow(Math.Abs(Math.Pow(x, 2) - Math.Pow(a, 2)), 0.2);
             return y;
         }
+
         public static void Main()
         {
+            double a = 2.0;
+            double b = 1.1;
+
             double[] z = new double[5] { 0.1, 0.3, 0.4, 0.45, 0.65 };
             Console.WriteLine("Задание А");
             for (double x = 0.08; x <= 1.08; x = x + 0.2)
             {
-                Console.WriteLine(Formula(x));
+                Console.WriteLine(Formula(x,a,b));
             }
 
             Console.WriteLine();
@@ -25,7 +28,7 @@ namespace ConsoleApp1
             foreach (int x in z)
             {
                 i ++;
-                Console.WriteLine(Formula(z[i]));
+                Console.WriteLine(Formula(z[i],a,b));
             }
 
           
