88ebdf9704f8e627e2511b799dc764bb9147bc5c
User of DK12 <DK12@it.isuct.ru>
2018-12-14 14:52:51 +0400

    Na proverku2

--- CourseApp.Tests/UnitTest1.cs
+++ CourseApp.Tests/UnitTest1.cs
@@ -1,36 +1,38 @@
 using System;
-using Xunit; 
-using ConsoleApp1;
+using Xunit;
 
+using ConsoleApp1;
 
 namespace CourseApp.Tests
 {
     public class UnitTest1
     {
-        double a = 2.0;
-        double b = 1.1;
-        double x1 = 0.08;
-        double x2 = 1.08;
-        double dx = 0.2;
-        double[] z = new double[5] { 0.1, 0.3, 0.4, 0.45, 0.65 };
+        private double x1 = 0.08;
+        private double x2 = 1.08;
+        private double dx = 0.2;
+        private double[] z = new double[5] { 0.1, 0.3, 0.4, 0.45, 0.65 };
+
         [Fact]
         public void Test1()
         {
             double y = Program.Formula(0.1, 2.0, 1.1);
             Assert.Equal(0.138, y, 3);
         }
+
         [Fact]
         public void Test2()
         {
             double y = Program.Formula(0, 2.0, 1.1);
             Assert.Equal(0.144, y, 3);
         }
+
         [Fact]
         public void Test3()
         {
             double y = Program.Formula(0.1, 0, 1.1);
             Assert.Equal(-0.025, y, 3);
         }
+
         [Fact]
         public void Test4()
         {
@@ -39,17 +41,18 @@ namespace CourseApp.Tests
         }
 
         [Fact]
-        public void test5()
+        public void Test5()
         {
             double[] mas = new double[5] { 0.138, 0.086, 0.037, 0.006, -0.185 };
             for (int i = 0; i < mas.Length; i++)
             {
-                var y = Program.Formula(z[i],2.0, 1.1);
+                var y = Program.Formula(z[i], 2.0, 1.1);
                 Assert.Equal(mas[i], y, 3);
             }
         }
+
         [Fact]
-        public void test6()
+        public void Test6()
         {
             int i = 0;
             double[] mas = new double[6] { 0.140, 0.094, -0.016, -0.226, -0.657, -2.544 };
@@ -60,6 +63,5 @@ namespace CourseApp.Tests
                 i++;
             }
         }
-        
     }
 }
--- CourseApp.Tests/UnitTest2.cs
+++ CourseApp.Tests/UnitTest2.cs
@@ -1,50 +1,46 @@
 using System;
-using Xunit; 
-using ConsoleApp1;
+using Xunit;
 
+using ConsoleApp1;
 
 namespace CourseApp.Tests
 {
     public class UnitTest2
     {
-
-        
         [Fact]
-        public void test7()
+        public void Test7()
         {
             Country country1 = new Country();
-            var people = country1.people;
-            var Square = country1.Square;
-            var Name = country1.Name;
+            var people = country1.People;
+            var square = country1.Square;
+            var name = country1.Name;
 
             Assert.Equal(24140800, people);
-            Assert.Equal(7692024, Square);
-            Assert.Equal("Australiya", Name);
-
+            Assert.Equal(7692024, square);
+            Assert.Equal("Australiya", name);
         }
 
         [Fact]
-        public void test8()
+        public void Test8()
         {
             Country country2 = new Country(30316, 12.6);
-            var people = country2.people;
-            var Square = country2.Square;
-            var Name = country2.Name;
+            var people = country2.People;
+            var square = country2.Square;
+            var name = country2.Name;
 
             Assert.Equal(30316, people);
-            Assert.Equal(12.6, Square);
-            Assert.Equal("Kohma", Name);
-
+            Assert.Equal(12.6, square);
+            Assert.Equal("Kohma", name);
         }
 
         [Fact]
         public void Test9()
         {
             Country country2 = new Country();
-            var Name = country2.Name;
-            Assert.Equal("Australiya", Name);
-            string Word = $"Strana {Name} skasala: \'Dhou!\', a mne nado pomenyat dillera";
-            Assert.Equal($"Strana Australiya skasala: \'Dhou!\', a mne nado pomenyat dillera", Word);
+            var name = country2.Name;
+            Assert.Equal("Australiya", name);
+            string word = $"Strana {name} skasala: \'Dhou!\', a mne nado pomenyat dillera";
+            Assert.Equal($"Strana Australiya skasala: \'Dhou!\', a mne nado pomenyat dillera", word);
         }
     }
 }
